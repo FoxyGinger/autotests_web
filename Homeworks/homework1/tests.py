4f45d29588ec85da77dce6282ad52c53b7b653a7
FoxyGinger <electrojanet@gmail.com>
2023-11-29 13:58:16 +0300

    homework1: tests descriptions added

--- Homeworks/homework1/tests.py
+++ Homeworks/homework1/tests.py
@@ -10,6 +10,14 @@ created_post_content = "some text"
 
 
 def test_posts(config, token):
+    """
+     Тест с использованием DDT проверяет наличие поста
+    с определенным заголовком в списке постов другого
+    пользователя, для этого выполняется get запрос по адресу
+    https://test-stand.gb.ru/api/posts c хедером, содержащим токен
+    авторизации в параметре "X-Auth-Token". Для отображения
+    постов другого пользователя передается "owner": "notMe"
+    """
     result = get(url=f"{config['url']}/{posts_path}", token=token, params={"owner": "notMe"})
     assert result.status_code == 200
     post_titles = [post['title'] for post in result.json()['data']]
@@ -17,6 +25,12 @@ def test_posts(config, token):
 
 
 def test_create_post(config, token):
+    """
+    Добавить в задание с REST API еще один
+    тест, в котором создается новый пост,
+    а потом проверяется его наличие на сервере
+    по полю “описание”.
+    """
     post_result = post(url=f"{config['url']}/{posts_path}", token=token, params={"title": created_post_title,
                                                                                  "description": created_post_description,
                                                                                  "content": created_post_content})
